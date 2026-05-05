d94956f4043d78e15ea718d0910dd9a07d886158
Shashank K <sharathshashank1986@gmail.com>
2026-05-05 16:43:00 +0530

    database creation

---
+++ instagram.sql
@@ -0,0 +1,21 @@
+create database instagram;
+
+use instagram;
+
+show databases;
+
+create table user_info(
+user_id char(5), user_name varchar(20), user_password varchar(10), gender enum('male','female','others')
+)
+
+insert into user_info(user_id, user_name, user_password, gender)
+values 
+('u101',"Shahshank","shashank","male"),
+('u102',"Harshitha","harshi","female"),
+('u103',"Keerthana","keerthi","female"),
+('u104',"Akash","akza","male");
+
+select * from user_info;
+
+
+
